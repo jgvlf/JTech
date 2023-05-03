2be1ca81b7f881afa7dba009075cef60222bfcd7
jgvlf <joaogabrielvlf@gmail.com>
2023-05-03 10:49:19 -0300

    #1 Change Initial Route

--- App.tsx
+++ App.tsx
@@ -8,7 +8,7 @@ export default function App() {
   const Stack = createNativeStackNavigator();
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName='Login'>
+      <Stack.Navigator initialRouteName='Home'>
         <Stack.Screen name="Home" component={HomePage} options={{headerShown: false}}/>
         <Stack.Screen name="Login" component={LoginPage} options={{headerShown: false}}/>
       </Stack.Navigator>
